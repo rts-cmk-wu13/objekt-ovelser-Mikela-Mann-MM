fc7dbda2af01793beb9ecc47b7d1649f753d6c31
mikelamann1@gmail.com <mikelamann1@gmail.com>
2025-02-03 10:38:01 +0100

    løsning opg1 tilføjet

---
+++ objekt.js
@@ -0,0 +1,12 @@
+// --- opgave 1 ---
+
+let person = {
+    fornavn: "Louise",
+    efternavn: "Pedersen",
+    stilling: "Front-end dev",
+    anciennitet: 3,
+
+}
+
+console.log(person.stilling);
+console.log(person.anciennitet);

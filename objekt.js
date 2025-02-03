3cde7e75992c69ecd9fda582cdcb2b05cda2c8bd
mikelamann1@gmail.com <mikelamann1@gmail.com>
2025-02-03 10:57:32 +0100

    løsning opgave 2 tilføjet

--- objekt.js
+++ objekt.js
@@ -10,3 +10,25 @@ let person = {
 
 console.log(person.stilling);
 console.log(person.anciennitet);
+
+// --- opgave 2 ---
+
+let person1 = {
+    fornavn: "Louise",
+    efternavn: "Pedersen",
+    stilling: "Front-end dev",
+    by: "København",
+    anciennitet: 3,
+    frokostordning: true, 
+    team: ["Karoline", "Mads", "Frederik", "Jens"],
+    erfaring: {
+        firma: "Lego",
+        varighed: 5
+    },
+}
+
+person1.team.forEach(function(team) {
+    console.log(team)
+});
+
+console.log(person1.erfaring.firma);
